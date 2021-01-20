cea13e29961daa16b3faa7eb080cccf3b4fc7e98
George Zacharia <george.zcharia@gmail.com>
2021-01-20 07:57:00 +0100

    clang-custom: Declare the toolchain path
    Change-Id: I9bcb5c100797bde3745ef5303d129d89f016b523

--- clang_custom.mk
+++ clang_custom.mk
@@ -1,3 +1,6 @@
 #
 # define the latest known clang version
 CLANG_CUSTOM_VERSION_LATEST := r399163b
+
+# define the custom clang path
+TARGET_KERNEL_CLANG_PATH := $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)_custom/clang-$(CLANG_CUSTOM_VERSION_LATEST)
